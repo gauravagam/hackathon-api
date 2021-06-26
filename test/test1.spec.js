5b13ffe6224ad9dce4f7178b71f0d42b5bb2639b
Manish Wadhwani <mwadhwani@iconnectsolutions.com>
2021-06-26 15:25:14 +0530

    added sample test

---
+++ test/test1.spec.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Manish Test', () => {
+    it('should display true', () => {
+        expect(true).to.be.equals(true);
+    });
+});
